fe427416442c5ffdbcd6266263db3a64d61ecac9
ckdev88 <ckdev88@outlook.com>
2023-08-15 13:35:15 -0300

    remove theme, in favour of terminal color profile

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -8,8 +8,4 @@ vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
 end)
