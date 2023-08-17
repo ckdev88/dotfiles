0e720116e76184b3563b8294da56e2e08e7d3658
ckdev88 <ckdev88@outlook.com>
2023-08-17 16:17:09 -0300

    move BufWritePre to ck lua folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,1 @@
 require('ck')
--- LSP, TODO: check if still necessary
-local augroup = vim.api.nvim_create_augroup
-local ckGroup = augroup('ck', {})
-local autocmd = vim.api.nvim_create_autocmd
-autocmd({ "BufWritePre" }, {
-        group = ckGroup,
-        pattern = "*",
-        command = [[%s/\s\+$//e]],
-})
--- nvim/lua/ck/init.lua
+++ nvim/lua/ck/init.lua
@@ -1,3 +1,12 @@
+-- LSP, TODO: check if still necessary
+local augroup = vim.api.nvim_create_augroup
+local ckGroup = augroup('ck', {})
+local autocmd = vim.api.nvim_create_autocmd
+autocmd({ "BufWritePre" }, {
+	group = ckGroup,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
 require('ck.base')
 require('ck.packer')
 require('ck.remap')
