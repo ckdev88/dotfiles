de2875c63fa92d905e75f78ba0745a3f2a228c1f
ckdev88 <ckdev88@outlook.com>
2023-08-22 20:17:52 -0300

    add installs

--- arch-distro-after-install.sh
+++ arch-distro-after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo systemctl disable geoclue.service;systemctl mask geoclue.service -l;
 sudo pacman -Syu
-sudo pacman -S krita gimp falkon librewolf ungoogled-chromium brave-bin firefox neovim alacritty micro gscan2pdf mpv sweethome3d okular xournalpp libreoffice-still dolphin btop meld
+sudo pacman -S krita gimp falkon librewolf ungoogled-chromium brave-bin firefox neovim alacritty micro gscan2pdf mpv sweethome3d okular xournalpp libreoffice-still dolphin btop meld github-cli freetube-bin
