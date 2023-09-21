279b562e76ca7ea3749a365585217ce7203e876a
ckdev88 <ckdev88@outlook.com>
2023-09-21 08:08:31 -0300

    prefer konsole over alacritty

--- arch-distro-after-install.sh
+++ arch-distro-after-install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo systemctl disable geoclue.service;systemctl mask geoclue.service -l;
 sudo pacman -Syu
-sudo pacman -S krita gimp falkon librewolf ungoogled-chromium brave-bin firefox neovim alacritty micro nvim vim kate gscan2pdf mpv sweethome3d okular xournalpp libreoffice-still dolphin xplr fzf btop meld github-cli freetube-bin
+sudo pacman -S krita gimp falkon librewolf ungoogled-chromium brave-bin firefox neovim konsole micro nvim vim kate gscan2pdf mpv sweethome3d okular xournalpp libreoffice-still dolphin xplr fzf btop meld github-cli freetube-bin
