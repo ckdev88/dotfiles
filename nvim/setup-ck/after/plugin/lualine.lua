8354f88b7035d63f7b29ff3c2b394ca0c22d973d
ckdev88 <ckdev88@outlook.com>
2023-09-28 20:19:06 -0300

    rm nvim files

--- nvim/setup-ck/after/plugin/lualine.lua
+++
@@ -1,8 +0,0 @@
-require('lualine').setup {
-  options = {
-    -- theme = 'gruvbox_dark',
-    section_separators = { left = '', right = '' },
-    component_separators = { left = '|', right = '|' },
-  }
-}
-
