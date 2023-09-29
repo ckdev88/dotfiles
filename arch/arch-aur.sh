240752c19cd454fb0d770cbeae960e235d664618
ckdev88 <ckdev88@outlook.com>
2023-09-29 19:48:52 -0300

    restructure files

--- arch/arch-aur.sh
+++ arch/arch-aur.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-#systemctl disable geoclue.service;systemctl mask geoclue.service -l;
 pacman -R micro
 pacman -Syu
 pacman -S krita gimp gwenview falkon librewolf ungoogled-chromium brave-bin firefox gscan2pdf mpv audacious sweethome3d okular xournalpp libreoffice-still btop 
