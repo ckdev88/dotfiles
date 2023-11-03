f9c88fe82190365587b9ab427900450086009043
ckdev88 <ckdev88@outlook.com>
2023-11-03 11:11:48 -0300

    add qutebrowser

--- arch/arch-aur.sh
+++ arch/arch-aur.sh
@@ -1,6 +1,6 @@
 pacman -R micro
 pacman -Syu
-pacman -S krita gimp gwenview falkon librewolf ungoogled-chromium brave-bin firefox gscan2pdf mpv audacious sweethome3d okular xournalpp libreoffice-still btop 
+pacman -S krita gimp gwenview falkon librewolf ungoogled-chromium brave-bin firefox qutebrowser gscan2pdf mpv audacious sweethome3d okular xournalpp libreoffice-still btop 
 pacman -S git clang ripgrep vim neovim kate fzf xplr meld konsole github-cli npm
 npm install -g prettier
 pacman -S xvkbd
