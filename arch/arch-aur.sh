52d992f0671d12a759c2de1e2c74ec5afcc2b476
ckdev88 <ckdev88@outlook.com>
2023-12-26 09:59:35 -0300

    dotfiles: add suckless, remove some kde

--- arch/arch-aur.sh
+++ arch/arch-aur.sh
@@ -1,6 +1,10 @@
 pacman -R micro
 pacman -Syu
-pacman -S krita gimp gwenview falkon librewolf ungoogled-chromium brave-bin firefox qutebrowser gscan2pdf mpv audacious sweethome3d okular xournalpp libreoffice-still btop 
-pacman -S git clang ripgrep vim neovim kate fzf xplr meld konsole github-cli npm
+pacman -S gwenview falkon librewolf ungoogled-chromium brave-bin firefox qutebrowser gscan2pdf mpv audacious sweethome3d okular xournalpp libreoffice-still btop 
+pacman -S git clang ripgrep vim neovim fzf xplr meld github-cli npm
 npm install -g prettier
 pacman -S xvkbd
+
+cd ~ && git clone https://git.suckless.org/surf && cd ~/surf && sudo make clean install && sudo rm ~/surf -r 
+
+cd ~ && git clone https://git.suckless.org/st && cd ~/st && sudo make clean install && sudo rm ~/st -r
