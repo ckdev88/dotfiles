ba5b5b0bd994c1a8350c80bf9a5a9447a9162334
ckdev88 <ckdev88@outlook.com>
2024-01-06 11:16:43 -0300

    suckless: reduce font size

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:style=Regular:size=12:antialias=true","monospace:size=12" };
+static const char *fonts[]          = { "Iosevka:style=Regular:size=10:antialias=true","monospace:size=10" };
 static const char dmenufont[]       = "Iosevka:style=Regular:size=10:antialias=true"; 
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -21,15 +21,14 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5" };
-
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 5,       0,           -1 },
+
+ 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
