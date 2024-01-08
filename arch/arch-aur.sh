fa7be910a8e5d71b96cea612c8c96db6af33444a
ckdev88 <ckdev88@outlook.com>
2024-01-08 14:13:00 -0300

    add unclutter, hide mouse pointer

--- arch/arch-aur.sh
+++ arch/arch-aur.sh
@@ -1,3 +1,4 @@
+
 pacman -R micro
 pacman -Syu
 pacman -S gwenview falkon librewolf ungoogled-chromium brave-bin firefox qutebrowser gscan2pdf mpv audacious sweethome3d okular xournalpp libreoffice-still btop 
@@ -10,4 +11,9 @@ cd ~ && git clone https://git.suckless.org/surf && cd ~/surf && sudo make clean 
 cd ~ && git clone https://git.suckless.org/st && cd ~/st && sudo make clean install && sudo rm ~/st -r
 
 pacman -S xbindkeys	xorg-xmodmap
+
+
+mkdir ~/tmp && cd ~/tmp
+git clone https://aur.archlinux.org/unclutter-xfixes-git.git && cd unclutter-xfixes-git && makepkg -si 
+
 
