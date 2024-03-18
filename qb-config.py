2e9822dea6c6c392b8a803f75bfd2c003d6fb72f
ckdev88 <ckdev88@outlook.com>
2024-03-18 07:41:06 -0300

    add qutebrowser config

---
+++ qb-config.py
@@ -0,0 +1,58 @@
+config_version: 2
+settings:
+  auto_save.session:
+    global: true
+  bindings.commands:
+    global:
+      normal:
+        q: null
+  colors.completion.even.bg:
+    global: '#000000'
+  colors.completion.item.selected.match.fg:
+    global: '#ee4444'
+  colors.completion.match.fg:
+    global: '#ee0000'
+  colors.completion.odd.bg:
+    global: '#111111'
+  colors.completion.scrollbar.bg:
+    global: '#000000'
+  colors.keyhint.suffix.fg:
+    global: '#dddd00'
+  colors.prompts.bg:
+    global: '#000000'
+  colors.tabs.bar.bg:
+    global: '#333333'
+  colors.tabs.even.bg:
+    global: '#000000'
+  colors.tabs.odd.bg:
+    global: '#000000'
+  colors.tabs.pinned.selected.even.bg:
+    global: black
+  colors.tabs.selected.even.bg:
+    global: '#dada00'
+  colors.tabs.selected.even.fg:
+    global: '#000000'
+  colors.tabs.selected.odd.bg:
+    global: '#dada00'
+  colors.tabs.selected.odd.fg:
+    global: '#000000'
+  colors.webpage.bg:
+    global: black
+  colors.webpage.darkmode.algorithm:
+    global: lightness-hsl
+  colors.webpage.darkmode.contrast:
+    global: 1.0
+  colors.webpage.darkmode.enabled:
+    global: true
+  colors.webpage.preferred_color_scheme:
+    global: dark
+  url.default_page:
+    global: https://wiby.me
+  url.searchengines:
+    global:
+      DEFAULT: https://yandex.com/search/?text={}
+      mojeek: https://www.mojeek.com/search?q={}
+      searx: https://searx.org/?q={}
+      wiby: https://www.wiby.me?q={}
+  url.start_pages:
+    global: about:blank
